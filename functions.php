a35db8d80d2c6ef4c2d335e046a6d56a8b0bc5b0
Taras Dashkevych <trdashix@gmail.com>
2018-11-27 13:40:12 -0800

    Remove: enqueue parent style function (now this style is loaded via the parent theme);

--- functions.php
+++ functions.php
@@ -18,13 +18,3 @@
      load_child_theme_textdomain( 'appetite', get_stylesheet_directory() . '/languages' );
  }
  add_action( 'after_setup_theme', 'appetite_child_theme_setup' );
-
-/**
- * Enqueue parent style.
- *
- * @link http://codex.wordpress.org/Child_Themes
- */
-function appetite_child_enqueue_parent_styles() {
-    wp_enqueue_style( 'appetite-parent-style', get_template_directory_uri() . '/style.css', array( 'appetite-css-framework' ) );
-}
-add_action( 'wp_enqueue_scripts', 'appetite_child_enqueue_parent_styles' );
